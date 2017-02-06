837ec29790cdcc52e08f5f6482ec8437fb509eac
Chodanun Srinil <step_nilanam@hotmail.com>
2017-02-06 15:02:56 +0700

    barcodeDBCompleted

--- makeup_lips_nyx.py
+++ makeup_lips_nyx.py
@@ -11,7 +11,7 @@ import os
 def main():
 	number_of_page = 1
 	max_page = 16
-	# file_name = open("mouse_name_barcode.txt", "w")
+	file_name = open("makeu_lips.csv", "w")
 
 	while number_of_page <= max_page:
 		url = "http://www.fishpond.co.nz/c/Beauty/Makeup/Lips/p/NYX?page="+str(number_of_page)
@@ -19,17 +19,28 @@ def main():
 		soup = BeautifulSoup(page.text, "html.parser")
 		links = soup.findAll(attrs={"class":"blue_link fn url"}) 
 		for i in links :
-			name = i.get_text().replace("'", " ")
+			name = i.get_text().replace("'", " ").replace("\""," ").replace("," ," ")
 			url_product = i.get('href')
 			page_product = requests.get(url_product)
 			soup = BeautifulSoup(page_product.text, "html.parser")
-			brand = soup.find(id="product_author").contents[1].get_text()
+			brand = soup.find(id="product_author").contents[1].get_text().replace("'"," ").replace("\""," ").replace("," ," ")
+			barcode_available = soup.find(attrs={"class":"product_info_text","width":"100%"}).contents[0].contents[2]
+			barcode = barcode_available.get("itemprop") == "gtin13" 
+			if barcode : 
+				barcode = barcode_available.get_text().strip()
+			else :
+				barcode = "9999999999999"
+			try:
+				description = soup.find(attrs={"class":"description"}).contents[1].get_text().strip()
+				description = description.replace("'"," ").replace("\""," ").replace("," ," ")
+			except Exception as e:
+				description = "no"
 			
-			print ("%s : %s"%(name,brand))
-			# file_name.write("%s\n"%i.get_text())
+			print ("%s,%s,%s,%s"%(name,brand,barcode,description))
+			file_name.write("%s,%s,%s,%s\n"%(name,brand,barcode,description))
 		print ("%d is downloaded"%(number_of_page))
 		number_of_page+=1
-		break
+	file_name.close()
 
 
 if __name__ == "__main__":
