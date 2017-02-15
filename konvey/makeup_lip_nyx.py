232d82824e35dd6694dce37987471ca0c5657ef5
Chodanun Srinil <step_nilanam@hotmail.com>
2017-02-15 22:04:52 +0700

    commentTable-rate,age

--- konvey/makeup_lip_nyx.py
+++ konvey/makeup_lip_nyx.py
@@ -20,9 +20,41 @@ def main():
 		soup = BeautifulSoup(page.text, "html.parser")
 		description = soup.find(attrs={"class":"pro_name"}).get_text().strip()
 		description = description[len(name):].strip()
+
+		# for comment table
+		ages = soup.findAll(attrs={"class":"right_text_skin"})
+		for age in ages:
+			try:
+				age = int(age.contents[3].get_text().split(" ")[1])
+			except Exception as e:
+				age = 0
+			# print("%s : %s"%(name,age))
+
+		rates = soup.findAll(attrs={"class":"f-right new_comm_stars"})
+		for j in rates :
+			try:
+				if j.contents[-2].get("class")[0].strip() != "" : # 5
+					rate = 5 
+				elif j.contents[-4].get("class")[0].strip() != "" : #4
+					rate = 4
+				elif j.contents[-6].get("class")[0].strip() != "" : #3
+					rate = 3
+				elif j.contents[-8].get("class")[0].strip() != "" : #3
+					rate = 2
+				elif j.contents[-10].get("class")[0].strip() != "" : #3
+					rate = 1
+				else :
+					rate = 0
+			except Exception as e:
+				rate = "-1"
+			
+			print("%s : %s"%(name,rate))
 		
-		print("%s : %s : %s : %s"%(name,brand,description,type_items))
-		break
+		# print("%s : %s : %s : %s"%(name,brand,description,type_items))
+		# comments = soup.find(attrs={"id":"commentlist"}).contents
+		# for j in comments :
+		
+		# break
 	
 	# file_name.close()
 
