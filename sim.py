d30d43b02bf82e8f76ef6499214fde30a7d3a14d
Chodanun Srinil <step_nilanam@hotmail.com>
2017-02-23 12:29:18 +0700

    demoUsing-Token_sort_ratio

--- sim.py
+++ sim.py
@@ -1,22 +1,31 @@
 import csv
 import editdistance
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 def readFile(path):
 	return open(path,'r')
 
-def editdistanceScore(name1, name2):
-	return editdistance.eval(name1, name2)
+def editdistance(name1, name2):
+	return editdistance.eval(name1, name2) # 0 -> good
+
+def token_sort_ratio(name1, name2):
+	return fuzz.token_sort_ratio(name1, name2)
 
 def sim(csvfile_barcode,csvfile_konvey):
+	point_max = 0
 	spamreader_barcode = csv.reader(csvfile_barcode, delimiter=',') # quotechar='|'
-	spamreader_konvey = csv.reader(csvfile_konvey, delimiter=',') # quotechar='|'
+	spamreader_konvey = csv.reader(csvfile_konvey, delimiter=',')
 	#barcode : 0,1:name,2:brand,3:barcode,4:description_eng,5:img,6:type
 	for row_konvey in spamreader_konvey:
 		name_konvey = row_konvey[1]
 		for row_barcode in spamreader_barcode:
 			name_barcode = row_barcode[1]
-			point = editdistanceScore(name_konvey,name_barcode)
-			print (point)
+			point = token_sort_ratio(name_konvey,name_barcode)
+			if point > point_max :
+				
+				print ("name_konvey : %s \nname_barcode : %s \npoint : %d\n\n"%(name_konvey,name_barcode,point))
+				point_max = max(point,point_max)
 		break
 
 def main():
@@ -24,9 +33,7 @@ def main():
 	csvfile_barcode = readFile('barcode/csv/barcode_database/lips-nyx.csv')
 	csvfile_konvey = readFile('konvey/csv/lip/nyx.csv')
 
-	# sim(csvfile_barcode,csvfile_konvey)
-	print (editdistance.eval('banana', 'banana'))
-	print (editdistance.eval('banana', 'asd'))
+	sim(csvfile_barcode,csvfile_konvey)
 
 if __name__ == '__main__' :
 	main()
