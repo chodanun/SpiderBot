5d23394698f1d6bae866976a3ef89e9e73677e1f
Chodanun Srinil <step_nilanam@hotmail.com>
2017-02-24 09:52:51 +0700

    beforeChangePattern-To-BarcodeBase

--- sim.py
+++ sim.py
@@ -12,28 +12,28 @@ def editdistance(name1, name2):
 def token_sort_ratio(name1, name2):
 	return fuzz.token_sort_ratio(name1, name2)
 
-def sim(csvfile_barcode,csvfile_konvey):
+def matching(csvfile_barcode,csvfile_items):
 	point_max = 0
 	spamreader_barcode = csv.reader(csvfile_barcode, delimiter=',') # quotechar='|'
-	spamreader_konvey = csv.reader(csvfile_konvey, delimiter=',')
+	spamreader_items = csv.reader(csvfile_items, delimiter=',')
 	#barcode : 0,1:name,2:brand,3:barcode,4:description_eng,5:img,6:type
-	for row_konvey in spamreader_konvey:
-		name_konvey = row_konvey[1]
+	for row_items in spamreader_items:
+		name_items = row_items[1]
 		for row_barcode in spamreader_barcode:
 			name_barcode = row_barcode[1]
-			point = token_sort_ratio(name_konvey,name_barcode)
+			point = token_sort_ratio(name_items,name_barcode)
 			if point > point_max :
 				barcode = row_barcode[3]
-				print ("name_konvey : %s \nname_barcode : %s \npoint : %d --> barcode : %s\n\n"%(name_konvey,name_barcode,point,barcode))
+				print ("name_items : %s \nname_barcode : %s \npoint : %d --> barcode : %s\n\n"%(name_items,name_barcode,point,barcode))
 				point_max = max(point,point_max)
 		break
 
 def main():
 	# same type & same brand
-	csvfile_barcode = readFile('barcode/csv/barcode_database/lips-nyx.csv')
-	csvfile_konvey = readFile('konvey/csv/lip/nyx.csv')
+	csvfile_barcode = readFile('../barcode.csv')
+	csvfile_items = readFile('../items.csv')
 
-	sim(csvfile_barcode,csvfile_konvey)
+	matching(csvfile_barcode,csvfile_items)
 
 if __name__ == '__main__' :
 	main()
