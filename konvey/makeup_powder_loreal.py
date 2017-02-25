2690500a589ef6979bcda019abdc25b96c13ee7d
Chodanun Srinil <step_nilanam@hotmail.com>
2017-02-25 22:22:26 +0700

    newData - Fix collection wrong

--- konvey/makeup_powder_loreal.py
+++ konvey/makeup_powder_loreal.py
@@ -7,8 +7,8 @@ from bs4 import BeautifulSoup
 def main():
 	file_items = open("csv/powder/loreal.csv", "w")
 	file_comment = open("csv/powder/loreal-comment.csv", "w")
-	item_id = 305
-	comment_id = 2652
+	item_id = 290
+	comment_id = 2209
 	brand = "L'Oreal"
 	type_items = "powder"
 	url = "http://loreal.konvy.com/search?brandDetailId=78&pid=114&cateId=2504"
@@ -18,13 +18,13 @@ def main():
 	links = soup.findAll(attrs={"class":"all_img"}) 
 	
 	for i in links :
-		name = i.contents[1].get("title").encode('utf-8').strip().replace("'"," ").replace('"',' ').replace(","," ")
+		name = i.contents[1].get("title").strip().replace("'"," ").replace('"',' ').replace(","," ")
 		url = i.get("href")
 		page = requests.get(url)
 		soup = BeautifulSoup(page.text, "html.parser")
 		description = soup.find(attrs={"class":"pro_name"}).get_text().strip()
-		description = description[len(i.contents[1].get("title").encode('utf-8').strip()):].encode('utf-8').strip().replace("'"," ").replace('"',' ').replace(","," ").replace("\n"," ")
-		img = i.contents[1].get('src').encode('utf-8')
+		description = description[len(i.contents[1].get("title").strip()):].strip().replace("'"," ").replace('"',' ').replace(","," ").replace("\n"," ")
+		img = i.contents[1].get('src')
 		file_items.write("%d,%s,%s,%s,%s,%s\n"%(item_id,name,brand,description,img,type_items))
 		# print ("%s,%s,%s,%s,%s\n"%(item_id,name,brand,description,type_items))
 		while True : # comment table (more than 1 page)
@@ -52,7 +52,7 @@ def main():
 				
 				try: # comment title 
 					if k.contents[1].get('class')[0] == "right_text_tit" :
-						comment_title = k.contents[1].get_text().encode('utf-8').strip().replace("'"," ").replace('"',' ').replace(","," ").replace("\n"," ")
+						comment_title = k.contents[1].get_text().strip().replace("'"," ").replace('"',' ').replace(","," ").replace("\n"," ")
 					else :
 						comment_title = ""	
 				except Exception as e:
@@ -60,9 +60,9 @@ def main():
 
 				try: # comment com
 					if k.contents[3].get('class')[0] == "right_text_con" : # comment
-						comment_com = k.contents[3].get_text().encode('utf-8').strip().replace("'"," ").replace("\""," ").replace(","," ").replace("\n"," ").replace('\r', '')
+						comment_com = k.contents[3].get_text().strip().replace("'"," ").replace("\""," ").replace(","," ").replace("\n"," ").replace('\r', '')
 					elif k.contents[1].get('class')[0] == "right_text_con" :
-						comment_com = k.contents[1].get_text().encode('utf-8').strip().replace("'"," ").replace("\""," ").replace(","," ").replace("\n"," ").replace('\r', '')
+						comment_com = k.contents[1].get_text().strip().replace("'"," ").replace("\""," ").replace(","," ").replace("\n"," ").replace('\r', '')
 					else :
 						comment_com = ""
 				except Exception as e:
@@ -75,7 +75,7 @@ def main():
 
 			next_page = soup.find(attrs={"class":"paginator"})	
 			try:
-				if next_page.contents[-2].contents[0].get_text().encode('utf-8').strip() == "Next" :
+				if next_page.contents[-2].contents[0].get_text().strip() == "Next" :
 					url = "http://www.konvy.com/"+next_page.contents[-2].contents[0].get('href')
 					page = requests.get(url)
 					soup = BeautifulSoup(page.text, "html.parser")
