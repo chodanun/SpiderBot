6712f11d409152aadcdb183c25111a24ccead8f0
Chodanun Srinil <step_nilanam@hotmail.com>
2017-02-26 02:38:22 +0700

    overall data - beta

--- test.py
+++ test.py
@@ -1,20 +1,5 @@
-#!/usr/bin/python
-#-*-coding: utf-8 -*-
-
-import requests
-import queue
-import re
-from urllib.parse import urljoin
-from bs4 import BeautifulSoup
-import os
-
-def main():
-	url = "http://www.fishpond.co.nz/Beauty/NARS-Satin-Lip-Pencil-Rikugien/9999962910175"
-	page = requests.get(url)
-	soup = BeautifulSoup(page.text, "html.parser")
-	img = soup.find("img",attrs={"class":"photo"}).get("src")
-	print ("%s"%(img))
-
-
-if __name__ == "__main__":
-	main()
+name = ['1','2','3','4','5']
+for i in name:
+	x = i
+	hello = "nat"
+print (hello)
